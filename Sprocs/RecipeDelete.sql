152b85285478e9645f015694416478615ac3fdd6
tobyrogovsky <tobyrogovsky@gmail.com>
2026-06-10 18:39:07 -0400

    recipedelete sproc

--- Sprocs/RecipeDelete.sql
+++ Sprocs/RecipeDelete.sql
@@ -1,14 +1,32 @@
 create or alter procedure dbo.RecipeDelete(
-@RecipeID int)
+@RecipeID int,
+@Message varchar (500) = '' output)
 as
 begin
+	declare @return int = 0
+	if exists (select * from recipe r where r.recipeID = @RecipeID and
+				(
+				r.PublishedDate is not null
+				and
+					(
+					    r.ArchivedDate is null
+					    or r.ArchivedDate >= dateadd(day,-30,getdate())
+					)
+				)
+			 )
+begin
+	 select @return = 1,
+               @Message = 'Recipe can only be deleted if it is drafted or archived for over 30 days.'
+        goto finished
+	end
 begin try
 	begin tran
-
-	delete RecipeIngredient where RecipeID = @RecipeID
-	delete Instruction where RecipeID = @RecipeID
-
-	delete Recipe where RecipeID = @RecipeID
+	
+        delete RecipeIngredient where RecipeID = @RecipeID
+        delete Instruction where RecipeID = @RecipeID
+        delete RecipeMealCourse where RecipeID = @RecipeID
+        delete CookBookRecipe where RecipeID = @RecipeID
+        delete Recipe where RecipeID = @RecipeID
 
 	commit
 end try
@@ -16,5 +34,7 @@ begin catch
 	rollback;
 	throw
 end catch
+finished: 
+return @return
 end
 go
